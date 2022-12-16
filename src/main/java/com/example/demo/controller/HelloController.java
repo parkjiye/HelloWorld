7e69cff0fe2c21f6ff315ce0d9168222cdf2b5d8
parkjiye <parkji3618@naver.com>
2022-12-16 22:36:36 +0900

    [FIX] POST -> GET #2

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-  @PostMapping("/hello")
+  @GetMapping("/hello")
   public String hello() {
     return "Hello World";
   }
